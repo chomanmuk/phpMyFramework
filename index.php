8d2289ff5f898f70948cc42381d516bb12687a56
조만묵 <jmm@jtnet.co.kr>
2020-05-04 13:55:42 +0900

    경로변경

--- index.php
+++ index.php
@@ -3,4 +3,4 @@ define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(dirname(__FILE__)));
 $url = $_GET['url'];
 
-require_once (ROOT.DS.'public_html'. DS . 'library' . DS . 'bootstrap.php');
+require_once (ROOT.DS.'public_html'. DS . 'librarys' . DS . 'bootstrap.php');
