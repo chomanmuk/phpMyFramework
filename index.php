2d58d82288025c0c7a2a3b0952b81a875e4a40dc
조만묵 <jmm@jtnet.co.kr>
2020-05-04 13:54:43 +0900

    public_html 경로 추가

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(dirname(__FILE__)));
 $url = $_GET['url'];
-print_r(ROOT);
-require_once (ROOT . DS . 'library' . DS . 'bootstrap.php');
+
+require_once (ROOT.DS.'public_html'. DS . 'library' . DS . 'bootstrap.php');
