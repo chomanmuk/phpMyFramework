2a596799d28387df52fed07700905e024019d4c8
조만묵 <jmm@jtnet.co.kr>
2020-05-04 14:13:48 +0900

    ㄴㅇㄹ

--- index.php
+++ index.php
@@ -2,8 +2,9 @@
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(dirname(__FILE__)).DS.'public_html');
 $url = $_GET['url'];
-print_r($url);
+
 if(!$url){
     $url="/main/view";
 }
+print_r($url);
 require_once (ROOT. DS . 'librarys' . DS . 'bootstrap.php');
