2497fb23ba82adc7528b44be2d055d771feea2f4
조만묵 <jmm@jtnet.co.kr>
2020-05-04 14:09:11 +0900

    공백일경우

--- index.php
+++ index.php
@@ -3,4 +3,7 @@ define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(dirname(__FILE__)).DS.'public_html');
 $url = $_GET['url'];
 print_r($url);
+if(!$url){
+    $url="/index";
+}
 require_once (ROOT. DS . 'librarys' . DS . 'bootstrap.php');
