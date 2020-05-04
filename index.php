e0b7a8bb477e373b856b667430029d1784d5a27d
조만묵 <jmm@jtnet.co.kr>
2020-05-04 13:57:56 +0900

    설정변경

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?
 define('DS', DIRECTORY_SEPARATOR);
-define('ROOT', dirname(dirname(__FILE__)));
+define('ROOT', dirname(dirname(__FILE__)).DS.'public_html');
 $url = $_GET['url'];
 
-require_once (ROOT.DS.'public_html'. DS . 'librarys' . DS . 'bootstrap.php');
+require_once (ROOT. DS . 'librarys' . DS . 'bootstrap.php');
