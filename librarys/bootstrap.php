b453a5bed08810055be01870f5b34d3164289457
조만묵 <jmm@jtnet.co.kr>
2020-05-04 15:20:09 +0900

    view 템플릿 추가

--- librarys/bootstrap.php
+++ librarys/bootstrap.php
@@ -2,4 +2,4 @@
 
 require_once (ROOT . DS . 'configs' . DS . 'config.php');
 require_once (ROOT . DS . 'librarys' . DS . 'shared.php');
-include_once (ROOT . DS . 'librarys' . DS . 'Template_.class.php');
+require_once (ROOT . DS . 'librarys' . DS . 'Template_.class.php');
