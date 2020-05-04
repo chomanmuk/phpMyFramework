8d59c7be59dcba3a1c731da47c707e49bcd320f6
조만묵 <jmm@jtnet.co.kr>
2020-05-04 14:38:34 +0900

    main controller 작성

---
+++ application/controllers/MainController.php
@@ -0,0 +1,7 @@
+<?php
+
+class MainController extends Controller {
+    function view(){
+        print_r(domain);
+    }
+}
--- configs/config.php
+++ configs/config.php
@@ -8,4 +8,5 @@ define('DB_NAME', 'phpproject');
 define('DB_USER', 'root');
 define('DB_PASSWORD', 'wmp1004!@#');
 define('DB_HOST', 'localhost');
-print_r($_SERVER["HTTP_HOST"]);
+define('domain', $_SERVER["HTTP_HOST"]);
+
