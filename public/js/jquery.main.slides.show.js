c4be933262dde078dff278552d1e17b59bc9c919
조만묵 <jmm@jtnet.co.kr>
2020-05-11 12:56:28 +0900

    no message

--- public/js/jquery.main.slides.show.js
+++ public/js/jquery.main.slides.show.js
@@ -100,8 +100,8 @@
 		},
 		fadeInAnimat : function(){
 			var self = this;
-			$(".box_skitter_data div[rel='" + self.settings.oldSlide + "']", self.slide).css({left:0+'px',width:100+'%'}).animate({opacity:0,zIndex:1, left:($(".box_skitter_data div[rel='" + self.settings.oldSlide + "']", self.slide).width()*0.5)+'px',width:120 +'%'}, self.settings.animation);
-			$(".box_skitter_data div[rel='" + self.settings.currentSlide + "']", self.slide).animate({opacity:1,zIndex:9,left:0+'px', width:100 +'%'}, self.settings.animation);
+			$(".box_skitter_data div[rel='" + self.settings.oldSlide + "']", self.slide).stop().animate({opacity:0,zIndex:1, left:'-'+($(".box_skitter_data div[rel='" + self.settings.oldSlide + "']", self.slide).width()*0.5)/2+'px',width:120 +'%'}, self.settings.animation);
+			$(".box_skitter_data div[rel='" + self.settings.currentSlide + "']", self.slide).css({left:0+'px',width:100+'%'}).stop().animate({opacity:1,zIndex:9,left:0+'px', width:100 +'%'}, self.settings.animation);
 
 		},
 		animate : function(){
