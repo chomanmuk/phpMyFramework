9219b6c158c21b58cd103b877e292c36f71d8494
조만묵 <jmm@jtnet.co.kr>
2020-05-13 15:30:09 +0900

    no message

--- public/js/jquery.main.slides.show.js
+++ public/js/jquery.main.slides.show.js
@@ -159,7 +159,7 @@
 			var step = 0;
 			for(var i=0;i<self.settings.widthcut;i++){
 				for(var j=0;j<self.settings.heightcut;j++){
-					$(".block[rel='" + step + "']", self.slide).stop().delay((step*50)).animate({opacity:0}, self.settings.animation, function(){ $(this).remove(); })
+					$(".block[rel='" + step + "']", self.slide).stop().delay(((i+j)*50)).animate({width:0+'px',height:0+'px',opacity:0}, { duration: self.settings.animation,  specialEasing: { width: "linear", height: "linear" } , complete: function(){ $(this).remove(); }})
 					step++;
 				}
 			}
@@ -169,7 +169,7 @@
 			var step = 0;
 			for(var i=0;i<self.settings.widthcut;i++){
 				for(var j=0;j<self.settings.heightcut;j++){
-					$(".block[rel='" + step + "']", self.slide).stop().delay(((i+j)*50)).animate({width:0+'px',height:0+'px'}, { duration: self.settings.animation,  specialEasing: { width: "linear", height: "linear" } , complete: function(){ $(this).remove(); }})
+					$(".block[rel='" + step + "']", self.slide).stop().delay(((i+j)*50)).animate({width:0+'px',height:0+'px',opacity:0}, { duration: self.settings.animation,  specialEasing: { width: "easeOutBounce", height: "easeOutBounce" } , complete: function(){ $(this).remove(); }})
 					step++;
 				}
 			}
