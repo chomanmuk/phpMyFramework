aba8d6ac0b488ce4830ad256aea663e3c62a9115
조만묵 <jmm@jtnet.co.kr>
2020-05-13 15:38:45 +0900

    no message

--- public/js/jquery.main.slides.show.js
+++ public/js/jquery.main.slides.show.js
@@ -176,7 +176,7 @@
 		},
 		closeMosaicReverseLayer : function(){
 			var self = this;
-			var step = ((self.settings.widthcut-1) * (self.settings.heightcut-1));
+			var step = (self.settings.widthcut * self.settings.heightcut) -1;
 			for(var i=0;i<self.settings.widthcut;i++){
 				for(var j=0;j<self.settings.heightcut;j++){
 					$(".block[rel='" + step + "']", self.slide).stop().delay(((i+j)*80)).animate({width:0+'px',height:0+'px',opacity:0}, { duration: self.settings.animation,  specialEasing: { width: "linear", height: "linear" } , complete: function(){ $(this).remove(); }})
