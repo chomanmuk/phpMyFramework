851f279b50a086a4987c3d9993c156393cde457c
조만묵 <jmm@jtnet.co.kr>
2020-05-13 14:27:21 +0900

    no message

--- public/js/jquery.main.slides.show.js
+++ public/js/jquery.main.slides.show.js
@@ -162,7 +162,7 @@
 		},
 		closeMosaicLayer : function(){
 			var self = this;
-			$(".box_skitter_data .block", self.slide).stop().animate({width:0+'px',height:0+'px'}, self.settings.animation, function(){ $(this).remove(); })
+			$(".box_skitter_data .block", self.slide).animate({width:0+'px',height:0+'px'}, self.settings.animation, function(){ $(this).remove(); })
 		},
 		blindAnimat : function(){
 			var self = this;
